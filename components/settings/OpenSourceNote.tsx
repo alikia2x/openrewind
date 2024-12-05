d5b333d5957dcea2070a907d2786f55cbb2eab36
alikia2x <alikia2x@outlook.com>
2024-12-06 00:56:54 +0800

    update: README & LICENSE

--- components/settings/OpenSourceNote.tsx
+++ components/settings/OpenSourceNote.tsx
@@ -5,7 +5,7 @@ export default function OpenSourceNote() {
 		<p id="settings-note" className="text-sm text-weaken mt-3">
 			<Trans i18nKey="settings.note">
 				OpenRewind is open source software licensed under
-				<a href="https://www.gnu.org/licenses/agpl-3.0.html">AGPL 3.0</a>.<br />
+				<a href="https://www.gnu.org/licenses/gpl-3.0.html">GPL 3.0</a>.<br />
 				Source code is avaliable at
 				<a href="https://github.com/alikia2x/openrewind">
 					GitHub
